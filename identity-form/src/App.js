5ca2eba89c5d0c103765e13c65eb3f1daab971f7
Lovefocux <olabodejoshuaf@gmail.com>
2022-03-15 08:48:14 +0100

    create forms

---
+++ identity-form/src/App.js
@@ -0,0 +1,13 @@
+import BioData from './components/BioData';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BioData/>
+      
+    </div>
+  );
+}
+
+export default App;
